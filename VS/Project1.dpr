c1951edf0a12310a29d08241cb1e856e29cf44cd
Thiago Domingos Venturim Ribeiro dos Santos <thiagodomingosventutim@gmail.com>
2026-01-22 19:08:28 -0300

    iniciando novo projeto de treino em delphin

---
+++ VS/Project1.dpr
@@ -0,0 +1,16 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  uPrincipal in 'uPrincipal.pas' {Form1},
+  uMenu in 'uMenu.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+
+end.
